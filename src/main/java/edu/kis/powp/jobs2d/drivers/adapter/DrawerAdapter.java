e5fc8ec89cb429899d6ed21191667629e7e62328
TinyRogue <oskarmikus@protonmail.com>
2021-03-03 13:43:50 +0100

    Renamed adapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
@@ -8,10 +8,10 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class checkIfRepoGOODAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawerAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public checkIfRepoGOODAdapter() {
+	public DrawerAdapter() {
 		super();
 	}
 
