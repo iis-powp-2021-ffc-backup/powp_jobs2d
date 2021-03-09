aa4fb4e6d9722614a2dbc162e3d5c4ab579c4eaf
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-03-09 19:28:35 +0100

    Marked error in AbstractDrawerAdapter

--- src/main/java/edu/kis/powp/jobs2d/drawer/AbstractDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drawer/AbstractDrawerAdapter.java
@@ -3,6 +3,10 @@ package edu.kis.powp.jobs2d.drawer;
 import edu.kis.powp.jobs2d.AbstractDriver;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
+/*
+* This kind of not working properly, due to not overrided setPosition method.
+* */
+@Deprecated
 public class AbstractDrawerAdapter extends AbstractDriver {
     private Job2dDriver driver;
 
