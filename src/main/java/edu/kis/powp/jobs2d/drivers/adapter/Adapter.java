79f162a100eae79e76890af31337f78fd9159206
adriankucharski <adrian1998.25@live.com>
2021-03-10 13:36:54 +0100

    Zmiana okna rysowania

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Adapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Adapter.java
@@ -4,6 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * TODO: Zmiana nazwy klasy Adapter
@@ -32,7 +33,7 @@ public class Adapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 		setPosition(x, y);
-		drawLine(line);
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
