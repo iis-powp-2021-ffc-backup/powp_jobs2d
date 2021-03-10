42108e1a6677512af12997037c333629609e1344
ImAStudentAtUniversity <215722@edu.p.lodz.pl>
2021-03-10 13:36:28 +0100

    Poprawa "parasolki"

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -10,6 +10,7 @@ import edu.kis.powp.jobs2d.Job2dDriver;
  */
 public class MyAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	ILine line;
 
 	public MyAdapter() {
 		super();
@@ -26,8 +27,8 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
 		drawLine(line);
+		setPosition(x, y);
 	}
 
 	@Override
