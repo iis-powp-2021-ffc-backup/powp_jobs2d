99a8e0931bb5f9453534bc6616069c5ce6d61662
Whitesan <piotrekrek12@wp.pl>
2021-03-10 13:24:55 +0100

    MyAdapter name changed, Figure repaired

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Adapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Adapter.java
@@ -8,10 +8,10 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class Adapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public Adapter() {
 		super();
 	}
 
@@ -26,8 +26,9 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
 		drawLine(line);
+
+		setPosition(x,y);
 	}
 
 	@Override
