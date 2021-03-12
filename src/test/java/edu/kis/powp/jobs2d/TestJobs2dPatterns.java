f8a0a9a9c15b456a6fe3b622e43464ac6d025ceb
1mbir <mikoimb99@gmail.com>
2021-03-12 21:26:14 +0100

    Dodanie możliwości korzystania z LineDrawerAdapter

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.RectangleAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
@@ -49,6 +50,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new RectangleAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver specialDriver = new LineDrawerAdapter();
+		DriverFeature.addDriver("Special Driver", specialDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
