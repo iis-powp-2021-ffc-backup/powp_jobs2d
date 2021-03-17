3218a05950c7452e6fbd729a54dc353cdcbba27a
Damian Wdowiak <damian.wdowiak@htdevelopers.com>
2021-03-17 13:43:52 +0100

    3.1.2 ComplexCommand

--- src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -1,4 +1,18 @@
 package edu.kis.powp.jobs2d.commands;
 
-public class ComplexCommand {
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+    private final List<DriverCommand> commands;
+
+    public ComplexCommand(List<DriverCommand> commands) {
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+        commands.forEach(c -> c.execute(job2dDriver));
+    }
 }
