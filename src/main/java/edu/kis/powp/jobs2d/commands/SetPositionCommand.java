6ba30fab5439f1d41837000e9fb3b91e2db02fde
Damian Wdowiak <damian.wdowiak@htdevelopers.com>
2021-03-17 13:04:08 +0100

    3.1.1, 3.1.2 basic commands implementation

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.commands;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand {
+    private final int x;
+    private final int y;
+
+    public SetPositionCommand(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.setPosition(x, y);
+    }
+}
