ab199f2da78c41d90c1527d07ffc740f7607c617
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-03-17 14:38:29 +0100

    added ComplexCommand

--- src/test/java/edu/kis/powp/command/DriverCommandTest.java
+++ src/test/java/edu/kis/powp/command/DriverCommandTest.java
@@ -1,6 +1,9 @@
 package edu.kis.powp.command;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DriverCommandTest
 {
     private static Job2dDriver driver = new StubDriver();
@@ -12,6 +15,17 @@ public class DriverCommandTest
         OperateToCommand operateToCommand = new OperateToCommand(7,7);
         setPositionCommand.execute(driver);
         operateToCommand.execute(driver);
+
+        List<DriverCommand> instructions = new ArrayList<>();
+
+        instructions.add(new SetPositionCommand(0,0));
+        instructions.add(new OperateToCommand(1,0));
+        instructions.add(new OperateToCommand(1,1));
+        instructions.add(new OperateToCommand(0,1));
+        instructions.add(new OperateToCommand(0,0));
+
+        ComplexCommand complexCommand = new ComplexCommand(instructions);
+        complexCommand.execute(driver);
     }
 
     private static class StubDriver implements Job2dDriver
@@ -27,7 +41,5 @@ public class DriverCommandTest
         {
             System.out.println("Driver operateTo x: " + x +" and y:"+ y);
         }
-
-
     };
 }
