eea3ca0bec52ca2cf5f0ee2785f3dc09206b9c1e
Damian Wdowiak <damian.wdowiak@htdevelopers.com>
2021-03-20 15:36:38 +0100

    3.1.6 ComplexCommand Builder

--- src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -2,6 +2,7 @@ package edu.kis.powp.jobs2d.commands;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand {
@@ -14,5 +15,18 @@ public class ComplexCommand implements DriverCommand {
     @Override
     public void execute(Job2dDriver job2dDriver) {
         commands.forEach(c -> c.execute(job2dDriver));
+    }
+
+    public static final class Builder {
+        private final List<DriverCommand> commands = new ArrayList<>();
+
+        public Builder addCommand(DriverCommand command) {
+            commands.add(command);
+            return this;
+        }
+
+        public ComplexCommand build() {
+            return new ComplexCommand(new ArrayList<>(commands));
+        }
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/factories/TriangleFigureComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/factories/TriangleFigureComplexCommand.java
@@ -1,23 +1,18 @@
 package edu.kis.powp.jobs2d.factories;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.commands.ComplexCommand;
-import edu.kis.powp.jobs2d.commands.DriverCommand;
 import edu.kis.powp.jobs2d.commands.OperateToCommand;
 import edu.kis.powp.jobs2d.commands.SetPositionCommand;
-
-import java.util.ArrayList;
 
 public class TriangleFigureComplexCommand implements FigureComplexCommand {
 
     @Override
     public ComplexCommand createComplexCommand() {
-        ArrayList<DriverCommand> commands = new ArrayList<>();
-        commands.add(new SetPositionCommand(-120, -120));
-        commands.add(new OperateToCommand(120, -120));
-        commands.add(new OperateToCommand(120, 120));
-        commands.add(new OperateToCommand(-120, -120));
-
-        return new ComplexCommand(commands);
+        return new ComplexCommand.Builder()
+                .addCommand(new SetPositionCommand(-120, -120))
+                .addCommand(new OperateToCommand(120, -120))
+                .addCommand(new OperateToCommand(120, 120))
+                .addCommand(new OperateToCommand(-120, -120))
+                .build();
     }
 }
