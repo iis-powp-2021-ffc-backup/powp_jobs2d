d9e3f994af741f86c172626025a7a156bc0aeb17
Blemm13 <kuba.kaniewski123@gmail.com>
2021-03-24 13:56:32 +0100

    minor fix

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -1,12 +1,12 @@
 package edu.kis.powp.command;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ComplexCommand implements DriverCommand{
 
-    private ArrayList<DriverCommand> driverCommands = new ArrayList<>();
+    private List<DriverCommand> driverCommands = new ArrayList<>();
 
     public void  queueCommand(DriverCommand driverCommand){
         driverCommands.add(driverCommand);
