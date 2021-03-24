7e3ea5e57b2ed6b35ad00eadd674c3693684be08
klima7 <ukasz.klimkiewicz@gmail.com>
2021-03-24 13:10:36 +0100

    3.1.5 Dodano fabrykę złożonych komend

---
+++ src/main/java/edu/kis/powp/command/CommandFactory.java
@@ -0,0 +1,24 @@
+package edu.kis.powp.command;
+
+public class CommandFactory {
+
+    public static DriverCommand getDrawRectangleCommand(int x, int y, int width, int height) {
+        ComplexCommand command = new ComplexCommand();
+        command.addCommand(new SetPositionCommand(x, y));
+        command.addCommand(new OperateToCommand(x+width, y));
+        command.addCommand(new OperateToCommand(x+width, y+height));
+        command.addCommand(new OperateToCommand(x, y+height));
+        command.addCommand(new OperateToCommand(x, y));
+        return command;
+    }
+
+    public static DriverCommand getDrawTriangleCommand(int x, int y, int width, int height) {
+        ComplexCommand command = new ComplexCommand();
+        command.addCommand(new SetPositionCommand(x, y));
+        command.addCommand(new OperateToCommand(x+width, y));
+        command.addCommand(new OperateToCommand(x+(width/2), y+height));
+        command.addCommand(new OperateToCommand(x, y));
+        return command;
+    }
+
+}
