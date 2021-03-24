29e792c522218d6f2fb8b8a9d5ececb479cca610
Karol Szczechowicz <222585@edu.p.lodz.pl>
2021-03-24 12:49:36 +0100

    Zaimplementowanie hierarchi klas Command

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+
+public class OperateToCommand implements DriverCommand{
+    
+    private int x;
+    private int y;
+
+    OperateToCommand(int x, int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+            
+    @Override
+    public void execute(Job2dDriver job2ddriver) {
+        job2ddriver.operateTo(x, y);
+    }  
+}
