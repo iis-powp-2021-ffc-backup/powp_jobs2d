10a2db79da12de20519aa919aed792909c7c7082
Tomek008 <tomasz.nykiel123@gmail.com>
2021-03-24 13:26:45 +0100

    Dodanie ComplexCommand

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,10 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class ComplexCommand implements DriverCommand{
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+
+    }
+}
