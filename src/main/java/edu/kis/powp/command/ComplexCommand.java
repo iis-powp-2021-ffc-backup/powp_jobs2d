ba2450f4a73f9217eeb9b7aa7d72c3c8cc3f9377
wiezawieza10 <wiezaawieza10@op.pl>
2021-03-31 22:38:58 +0200

    small fixes / code cleaning

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -1,7 +1,6 @@
 package edu.kis.powp.command;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
-
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand {
--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -2,18 +2,17 @@ package edu.kis.powp.command;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-public class OperateToCommand implements DriverCommand{
+public class OperateToCommand implements DriverCommand {
     private int x;
     private int y;
-
 
     public OperateToCommand(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     @Override
     public void execute(Job2dDriver driver) {
-        driver.operateTo(this.x,this.y);
-
+        driver.operateTo(this.x, this.y);
     }
 }
